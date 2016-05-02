8a3c3ee1cc44a5ac0de6d359c88f7408f3b17c08
Taylor Zelaya <taylormzelaya@gmail.com>
2016-05-02 15:01:24 -0700

    transferred over Max's notes on controller purpose

--- js-skeleton/control.js
+++ js-skeleton/control.js
@@ -1,5 +1,16 @@
 /* 'CONTROL' FUNCTIONALITY POSSIBLY NEEDED FOR APP
 
+Ingredient.createTable();
 
+We only run model or view when CONTROLLER tells us to
+Only runs when page tells it to
+
+For ABOUT (static), you'd just call aboutView.showAbout / aboutView.showHide (or whatever)
+
+Could have hideShow function in generic app.js
+
+Page controls Controller controls Model/View
+
+Check that Recipe.all has recipes in it before enlarging one recipe
 
 */
