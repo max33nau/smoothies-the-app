778123613ed26de6b57a1cf4101bdc81c016913c
Taylor Zelaya <taylormzelaya@gmail.com>
2016-05-02 15:22:14 -0700

    MVC schemas complete; time to write the logic

--- js-skeleton/control.js
+++ js-skeleton/control.js
@@ -1,11 +1,14 @@
 /* 'CONTROL' FUNCTIONALITY POSSIBLY NEEDED FOR APP
 
+***PAGE controls CONTROLLER controls MODEL/VIEW***
+
 ***********************************************************************************************
 createController.js
 
 createView.renderPage();
 
 ***********************************************************************************************
+
 
 ***********************************************************************************************
 recipesController.js
@@ -14,6 +17,7 @@ recipesView.renderPage();
 
 ***********************************************************************************************
 
+
 ***********************************************************************************************
 findController.js
 
@@ -21,24 +25,12 @@ findView.renderPage();
 
 ***********************************************************************************************
 
+
 ***********************************************************************************************
 aboutController.js
 
 aboutView.renderPage();
 
 ***********************************************************************************************
-
-Ingredient.createTable();
-
-We only run model or view when CONTROLLER tells us to
-Only runs when page tells it to
-
-For ABOUT (static), you'd just call aboutView.showAbout / aboutView.showHide (or whatever)
-
-Could have hideShow function in generic app.js
-
-Page controls Controller controls Model/View
-
-Check that Recipe.all has recipes in it before enlarging one recipe
 
 */
