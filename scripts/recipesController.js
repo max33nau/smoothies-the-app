cd24bf6a2742bfdb9f5ec05d5da7dcca99aafc95
Taylor Zelaya <taylormzelaya@gmail.com>
2016-05-02 16:15:40 -0700

    fix #11

--- scripts/recipesController.js
+++ scripts/recipesController.js
@@ -3,6 +3,7 @@
   var recipesController = {};
 
   recipesController.index = function(ctx, next) {
+    // *SHOULD CHECK TO SEE IF (ctx.params.recipeName) TO KNOW WHETHER TO SHOW A FULL RECIPE
     recipesView.renderPage();
   };
 
---
+++ scripts/route.js
@@ -0,0 +1,17 @@
+(function(module) {
+  var router = {};
+
+  router.setMapping = function() {
+    page.base('');
+
+    page('', createController.index);
+    page('/create', '/');
+    page('/about', aboutController.index);
+    page('/find', findController.index);
+    page('/recipes', recipesController.index);
+    page('/recipes/:recipeName', recipesController.index);
+
+    page();
+  };
+
+}(window));
