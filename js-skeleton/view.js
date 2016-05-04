e1ea8cb24988c2222659fb8aff9f654961549786
Taylor Zelaya <taylormzelaya@gmail.com>
2016-05-04 09:18:05 -0700

    move Create view  functionality from skel into createView.js

--- js-skeleton/view.js
+++ js-skeleton/view.js
@@ -1,23 +1,6 @@
 /* 'VIEW' FUNCTIONALITY POSSIBLY NEEDED FOR APP
 
-var createView = {};
 
-createView.populateFilters = function() {};  //populate dietary goal filter(s)
-
-createView.handleFilters = function() {};  //when user selects filter,
-                                           //highlight ingredients accordingly
-
-createView.showFullLabel = function() {};  //show (or create) full label popup
-
-createView.hideFullLabel = function() {};  //hide (or remove) full label popup
-
-createView.saveRecipe = function() {};  //event handler for Save Recipe button
-
-createView.resetPage = function() {};  //deselect all ingredients, set quantities to 0
-
-createView.renderPage = function() {  //render page and set event handlers
-  $('#createContent').show().siblings().hide();
-};
 
 ***********************************************************************************************
 
