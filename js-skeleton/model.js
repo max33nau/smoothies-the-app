fac82db299cc717a86a209270d312c9f62901513
Taylor Zelaya <taylormzelaya@gmail.com>
2016-05-04 09:14:04 -0700

    move Create model functionality from skel into create.js

--- js-skeleton/model.js
+++ js-skeleton/model.js
@@ -1,19 +1,6 @@
 /* 'MODEL' FUNCTIONALITY POSSIBLY NEEDED FOR APP
 
-var Ingredient = function() {};  //object constructor
 
-Ingredient.all = [];  //to hold all Ingredient objects
-
-Ingredient.createTable = function() {};  //create ingredients table in DB if not exists
-
-Ingredient.insertTableRow = function() {};  //insert row in ingredients table for given Ingredient
-
-Ingredient.createAll = function() {};  //create Ingredient object for each row in DB
-                                       //and push to Ingredient.all array
-
-Ingredient.retrieveAll = function() {};  //retrieve data from ingredients DB if rows.length
-                                         //else fetch USDA data and create Ingredients, rows
-                                         //in either case, call Ingredient.createAll
 
 ***********************************************************************************************
 
--- scripts/create.js
+++ scripts/create.js
@@ -1,4 +1,18 @@
 (function(module) {
 
+  var Ingredient = function() {};  //object constructor
+
+  Ingredient.all = [];  //to hold all Ingredient objects
+
+  Ingredient.createTable = function() {};  //create ingredients table in DB if not exists
+
+  Ingredient.insertTableRow = function() {};  //insert row in ingredients table for given Ingredient
+
+  Ingredient.createAll = function() {};  //create Ingredient object for each row in DB
+                                         //and push to Ingredient.all array
+
+  Ingredient.retrieveAll = function() {};  //retrieve data from ingredients DB if rows.length
+                                           //else fetch JSON data and create Ingredients, rows
+                                           //in either case, call Ingredient.createAll
 
 }(window));
