467fdc44599cf43ef264f9d5444e1ac35a9c2f7b
Taylor Zelaya <taylormzelaya@gmail.com>
2016-05-04 10:22:55 -0700

    add skeleton to findView.js, incl handleSearchButton()

--- scripts/findView.js
+++ scripts/findView.js
@@ -1,4 +1,13 @@
 (function(module) {
 
+  var findView = {};
+
+  //set event handler for search button - query Google Maps API to find smoothie shops near user-inputted address
+  findView.handleSearchButton = function() {};
+
+  //render Find section and hide other "page" sections
+  findView.renderPage = function() {
+    $('#findContent').show().siblings().hide();
+  };
 
 }(window));
