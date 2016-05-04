0f6afe280ad319af2ce9a0c9b368c78e7650ffbd
Kim Feinberg <kimberly380@gmail.com>
2016-05-03 18:41:12 -0700

    added json objects for ingredients and nutrients

---
+++ scripts/nutrient.js
@@ -0,0 +1,130 @@
+var nutrientList = [  
+  {  
+    "Nutrient":"Water",
+    "unitMeasure":"g"
+  },
+  {  
+    "Nutrient":"Calories",
+    "unitMeasure":"kcal"
+  },
+  {  
+    "Nutrient":"Protein",
+    "unitMeasure":"g"
+  },
+  {  
+    "Nutrient":"Lipids",
+    "unitMeasure":"g"
+  },
+  {  
+    "Nutrient":"Carbohydrate",
+    "unitMeasure":"g"
+  },
+  {  
+    "Nutrient":"Fiber",
+    "unitMeasure":"g"
+  },
+  {  
+    "Nutrient":"Sugars",
+    "unitMeasure":"g"
+  },
+  {  
+    "Nutrient":"Calcium",
+    "unitMeasure":"mg"
+  },
+  {  
+    "Nutrient":"Iron",
+    "unitMeasure":"mg"
+  },
+  {  
+    "Nutrient":"Magnesium",
+    "unitMeasure":"mg"
+  },
+  {  
+    "Nutrient":"Phosphorus",
+    "unitMeasure":"mg"
+  },
+  {  
+    "Nutrient":"Potassium",
+    "unitMeasure":"mg"
+  },
+  {  
+    "Nutrient":"Sodium",
+    "unitMeasure":"mg"
+  },
+  {  
+    "Nutrient":"Zinc",
+    "unitMeasure":"mg"
+  },
+  {  
+    "Nutrient":"Copper",
+    "unitMeasure":"mg"
+  },
+  {  
+    "Nutrient":"Manganesium",
+    "unitMeasure":"mg"
+  },
+  {  
+    "Nutrient":"Vitamin C",
+    "unitMeasure":"mg"
+  },
+  {  
+    "Nutrient":"Thiamin",
+    "unitMeasure":"mg"
+  },
+  {  
+    "Nutrient":"Riboflavin",
+    "unitMeasure":"mg"
+  },
+  {  
+    "Nutrient":"Niacin",
+    "unitMeasure":"mg"
+  },
+  {  
+    "Nutrient":"Vitamin B6",
+    "unitMeasure":"mg"
+  },
+  {  
+    "Nutrient":"Folic Acid",
+    "unitMeasure":"mcg"
+  },
+  {  
+    "Nutrient":"Vitamin B12",
+    "unitMeasure":"mcg"
+  },
+  {  
+    "Nutrient":"Vitamin A",
+    "unitMeasure":"IU"
+  },
+  {  
+    "Nutrient":"Beta Carotine",
+    "unitMeasure":"mcg"
+  },
+  {  
+    "Nutrient":"Vitamin E",
+    "unitMeasure":"mg"
+  },
+  {  
+    "Nutrient":"Vitamin D",
+    "unitMeasure":"IU"
+  },
+  {  
+    "Nutrient":"Vitamin K",
+    "unitMeasure":"mcg"
+  },
+  {  
+    "Nutrient":"Fat: Saturated",
+    "unitMeasure":"g"
+  },
+  {  
+    "Nutrient":"Fat: Monounsaturated",
+    "unitMeasure":"g"
+  },
+  {  
+    "Nutrient":"Fat: Polyunsaturated",
+    "unitMeasure":"g"
+  },
+  {  
+    "Nutrient":"Cholesterol",
+    "unitMeasure":"mg"
+   }
+]
