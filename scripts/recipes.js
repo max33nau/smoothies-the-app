42c21868e7880c2a77627b1f22abc861b1281c64
Taylor Zelaya <taylormzelaya@gmail.com>
2016-05-04 09:15:28 -0700

    move Recipes model functionality from skel into recipes.js

--- scripts/recipes.js
+++ scripts/recipes.js
@@ -1,4 +1,17 @@
 (function(module) {
 
+  var Recipe = function() {};  //object constructor
+
+  Recipe.all = [];  //to hold all Recipe objects
+
+  Recipe.createTable = function() {};  //create recipes table in DB if not exists
+
+  Recipe.insertTableRow = function() {};  //insert row in recipes table for given Recipe
+
+  Recipe.createAll = function() {};  //create Recipe object for each row in DB
+                                     //and push to Recipe.all array
+
+  Recipe.retrieveAll = function() {};  //retrieve data from recipes DB if rows.length
+                                       //call Recipe.createAll
 
 }(window));
