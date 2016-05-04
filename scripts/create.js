e2e1c18fd612eec808e44b3c1bef397618c6f861
Taylor Zelaya <taylormzelaya@gmail.com>
2016-05-04 13:56:04 -0700

    clean up updated create.js

--- scripts/create.js
+++ scripts/create.js
@@ -1,28 +1,29 @@
 (function(module) {
 
-//array of nutrient names used to create final 'nutrient:' property in object
-
-  var arrNutrients = ["Water", "Calories", "Protein", "Lipids", "Carbohydrate", "Fiber", "Sugars", "Calcium", "Iron", "Magnesium", "Phosphorus", "Potassium", "Sodium", "Zinc", "Copper", "Manganesium", "Vitamin C", "Thiamin", "Riboflavin", "Niacin", "Vitamin B6", "Folic Acid", "Vitamin B12", "Vitamin A", "Beta Carotine", "Vitamin E", "Vitamin D", "Vitamin K", "Fat: Saturated", "Fat: Monounsaturated", "Fat: Polyunsaturated", "Cholesterol"];
+  //array of nutrient names used to create final 'nutrient' property of ingredient objects
+  var arrNutrients = ['Water', 'Calories', 'Protein', 'Lipids', 'Carbohydrate', 'Fiber', 'Sugars', 'Calcium', 'Iron', 'Magnesium', 'Phosphorus', 'Potassium', 'Sodium', 'Zinc', 'Copper', 'Manganesium', 'Vitamin C', 'Thiamin', 'Riboflavin', 'Niacin', 'Vitamin B6', 'Folic Acid', 'Vitamin B12', 'Vitamin A', 'Beta Carotine', 'Vitamin E', 'Vitamin D', 'Vitamin K', 'Fat: Saturated', 'Fat: Monounsaturated', 'Fat: Polyunsaturated', 'Cholesterol'];
 
 
   var Ingredient = {};
 
-  //to hold all Ingredient objects
+  //to hold all ingredient objects
   Ingredient.all = [];
 
-  //grabbing data sets that need to be compiled into final array 
+  //grabbing data sets that need to be compiled into final array
   Ingredient.compileAll = function(){
     $.getJSON('dataFiles/ingred.json').done(function(data){
       Ingredient.rawData = data;
-  
+
       $.getJSON('dataFiles/nutrient-v2.json').done(function(data){
-        Ingredient.nutrientData = data[0];  
-    
-        Ingredient.createArray(Ingredient.rawData, Ingredient.nutrientData);  //calling funciton to create array
+        Ingredient.nutrientData = data[0];
+
+        //calling function to create array
+        Ingredient.createArray(Ingredient.rawData, Ingredient.nutrientData);
       });
     });
   };
-//method to pull data from various datasets into a single array of ingredient objects, with one nutrient property that is an array.
+
+  //method to pull data from various datasets into a single array of ingredient objects, with one nutrient property that is an array.
   Ingredient.createArray = function (ingredients, nutrients){
     Ingredient.all = ingredients.map(function(obj){
       var newIngredObj = {};
@@ -39,13 +40,11 @@
         return nutrientObj;
       });
       return newIngredObj;
-    });  
+    });
   };
-  
+
+  /******!!!!!!!!!!!!!!!!!!!!!!!!!!!!!TO BE REMOVED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*****/
   Ingredient.compileAll();
-  
+
   module.Ingredient = Ingredient;
 }(window));
-
-
-
