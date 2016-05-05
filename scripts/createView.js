890c9ef319399b4eea933ea1266f27a15809964a
Kim Feinberg <kimberly380@gmail.com>
2016-05-05 02:03:19 -0700

    added label to dom in very manual way.  It's all about gettin-shit-done at this point!

--- scripts/createView.js
+++ scripts/createView.js
@@ -1,7 +1,8 @@
 (function(module) {
 
   var createView = {};
-
+  
+  
   //object to store current recipe ingredients and nutrition information
   createView.currentRecipe = {};
 
@@ -26,67 +27,70 @@
     var selectedArrayText = [];
     var $qtySelected = $('#qtySelected');
     var $nutrientList = $('#nutrientList ul');
-    var $labelTemplate = $('#labelTemplate');
-    var compiledLabel = Handlebars.compile($labelTemplate.html());
-    
-    
+    var selectObject = {}; //object to capture user selections
+        
     $accordion.on('click','li',function(){
       var selectedText = $(this).text().split(":",1)[0];
       selectedArrayText.push(selectedText);
-      // $nutrientList.append('<li>' + selectedText + '</li>');
-      // $qtySelected.text(function(i, val){
-      //   return +val+1});
-   
-      // Ingredient.all.forEach(function(object){
-      //   >-1){
-      var emptyObject = {};
+      $nutrientList.append('<li>' + selectedText + '</li>');
+    
+      
       Ingredient.all.forEach(function(object){
-       if(selectedArrayText.indexOf(object.name)>-1){
+        if(selectedArrayText.indexOf(object.name)>-1){
           object.nutrient.forEach(function(nutrientObj){
-            if(emptyObject[nutrientObj.name]){
-              emptyObject[nutrientObj.name]+=nutrientObj.qty;
+            if(selectObject[nutrientObj.name]){
+              selectObject[nutrientObj.name]+=nutrientObj.qty;
             } else{
-              emptyObject[nutrientObj.name]=0;
-              emptyObject[nutrientObj.name]+=nutrientObj.qty;
+              selectObject[nutrientObj.name]=0;
+              selectObject[nutrientObj.name]+=nutrientObj.qty;
             } 
           });
         }  
       });
-      // console.log(emptyObject.Water);
-          //the below function sorts the nutrients biggest to smallest and outputs an array.
-          var keys = []          
-          Object.keys(emptyObject)
-            .map(function (k) {return [k, emptyObject[k]]; })
-            .sort(function (b,a) {
-              if(a[1]> b[1]) return 1;
-              if(a[1]< b[1]) return -1;
-              return 0;   
-            }).forEach(function(d){
-              keys.push(d[0],d[1])
-            });
-            topTen = keys.slice(0,20);
-            console.log(topTen);
-              
-        
-          
-           
-            
-      console.log(emptyObject, selectedArrayText);
+      
+      //the below function sorts the nutrients biggest to smallest and outputs an array. Currently not being used.
+      // var keys = [];
+      // var vals = [];      
+      // Object.keys(selectObject)
+      //   .map(function (k) {return [k, selectObject[k]]; })
+      //   .sort(function (b,a) {
+      //     if(a[1]> b[1]) return 1;
+      //     if(a[1]< b[1]) return -1;
+      //     return 0;   
+      //   }).forEach(function(d){
+      //     keys.push(d[0]);
+      //     vals.push(d[1]);
+      //   });
+      // topTenKeys = keys.slice(0,10);
+      // topTenVals = vals.slice(0,10);
+      // console.log(topTenKeys, topTenVals);
+       
+       
+      //append to dom   
+      $('#Calories').text(selectObject.Calories);
+      // $('#Fat').text(selectObject.Calories);
+      // $('#Fat-Saturated').text(selectObject);
+      $('#Carbohydrate').text(selectObject.Carbohydrate);
+      $('Fiber').text(selectObject.Fiber);
+      $('#Sugars').text(selectObject.Sugars);
+      $('#Vitamin D').text(selectObject['Vitamin D']);
+      $('#Calcium').text(selectObject.Calcium);
+      $('#Iron').text(selectObject.Iron);
+      $('#Potassium').text(selectObject.Potassium);
+      $('#Vitamin A').text(selectObject['Vitamin A']);
+      $('#Vitamin C').text(selectObject['Vitamin C']);
+      
+      console.log(selectObject, selectedArrayText);
     });
-      
-      
   }; 
   
-              
-  //need to take calories and fats out of array then reduce to top 10, then do handlebars.
-           
-                    
+       
 
   //when ingredient added or removed or quantity changed, update ingredients AND nutrition info
   createView.updateCurrentRecipe = function() {};
 
   //render partial nutrition label based on latest currentRecipe info
-  createView.renderPartialLabel = function() {};
+  createView.renderPartialLabel = function() {};//see on click
 
   //show (or create) full label popup based on latest currentRecipe info
   createView.showFullLabel = function() {};
@@ -98,7 +102,13 @@
   createView.saveRecipe = function() {};
 
   //deselect all ingredients, set quantities to 0
-  createView.resetPage = function() {};
+  createView.resetPage = function() {
+    var $btnReset = $('#btnReset');
+    
+    $btnReset.on('click',function() {
+      ///empty selectObject {}
+    });
+  };
 
   //show Create section, hiding all other "page" sections
   createView.renderPage = function() {
@@ -116,7 +126,7 @@
   $('#recipesContent').hide();
 
   createView.handleSelectedIngredients();
-
+  createView.resetPage();
 
   module.createView = createView;
 }(window));
